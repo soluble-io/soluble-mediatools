75c6c4558b536cdadf32e8df01d4f2f0e90e1f8f
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-06 12:50:57 +0200

    added more tests

--- tests/functional/Recipes/VideoSimpleConversionTest.php
+++ tests/functional/Recipes/VideoSimpleConversionTest.php
@@ -8,10 +8,13 @@ use MediaToolsTest\TestUtilTrait;
 use PHPUnit\Framework\TestCase;
 use Soluble\MediaTools\Exception\FileNotFoundException;
 use Soluble\MediaTools\Exception\ProcessConversionException;
+use Soluble\MediaTools\Filter\Video\EmptyVideoFilter;
+use Soluble\MediaTools\Filter\Video\VideoFilterChain;
+use Soluble\MediaTools\Filter\Video\YadifVideoFilter;
 use Soluble\MediaTools\VideoConvert;
 use Soluble\MediaTools\VideoConvertParams;
 
-class VideoConversionTest extends TestCase
+class VideoSimpleConversionTest extends TestCase
 {
     use TestUtilTrait;
 
@@ -62,6 +65,70 @@ class VideoConversionTest extends TestCase
         self::assertFileExists($outputFile);
         unlink($outputFile);
     }
+
+    public function testFullOptions(): void
+    {
+        $outputFile = "{$this->outputDir}/testFullOptions.output.webm";
+
+        if (file_exists($outputFile)) {
+            unlink($outputFile);
+        }
+
+        $videoFilterChain = new VideoFilterChain();
+        $videoFilterChain->addFilter(new EmptyVideoFilter());
+        $videoFilterChain->addFilter(new YadifVideoFilter());
+
+
+        $convertParams = (new VideoConvertParams())
+            ->withVideoCodec('libvpx-vp9')
+            //->withCrf(32) - Using variable bitrate instead:
+            ->withVideoBitrate('200k') // target bitrate
+            ->withVideoMaxBitrate('250000') // max bitrate
+            ->withVideoMinBitrate('150k') // min bitrate
+            ->withAudioCodec('libopus')
+            ->withAudioBitrate('96k')
+            ->withVideoFilter($videoFilterChain)
+            ->withThreads(0) // 0 means threads = number of cores
+            ->withSpeed(8)
+            ->withKeyframeSpacing(240)
+            ->withTileColumns(1)
+            ->withFrameParallel(1)
+            ->withPixFmt('yuv420p')
+            ->withOutputFormat('webm');
+
+        self::assertFileExists($this->videoFile);
+        self::assertFileNotExists($outputFile);
+
+        try {
+            $this->videoConvert->convert($this->videoFile, $outputFile, $convertParams);
+        } catch (ProcessConversionException | FileNotFoundException $e) {
+            self::fail(sprintf('Failed to convert video: %s', $e->getMessage()));
+        }
+
+        self::assertFileExists($outputFile);
+        unlink($outputFile);
+
+        // Check the outputed command
+        $process = $this->videoConvert->getConversionProcess($this->videoFile, $outputFile, $convertParams);
+        $cmdLine = $process->getCommandLine();
+
+        self::assertContains(' -vcodec libvpx-vp9 ', $cmdLine);
+        self::assertContains(' -b:v 200k ', $cmdLine);
+        self::assertContains(' -maxrate 250000', $cmdLine);
+        self::assertContains(' -minrate 150k ', $cmdLine);
+        self::assertContains(' -acodec libopus ', $cmdLine);
+        self::assertContains(' -b:a 96k ', $cmdLine);
+        self::assertContains(' -vf yadif=mode=0:parity=-1:deint=0 ', $cmdLine);
+        self::assertContains(' -threads 0 ', $cmdLine);
+        self::assertContains(' -speed 8 ', $cmdLine);
+        self::assertContains(' -g 240 ', $cmdLine);
+        self::assertContains(' -tile-columns 1 ', $cmdLine);
+        self::assertContains(' -frame-parallel 1', $cmdLine);
+        self::assertContains(' -pix_fmt yuv420p', $cmdLine);
+        self::assertContains(' -f webm ', $cmdLine);
+
+    }
+
 
     public function testConvertMustThrowFileNotFoundException(): void
     {
