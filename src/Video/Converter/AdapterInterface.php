a4a4c481cf95f14057a2c6436f539321454d9760
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-08 13:18:37 +0200

    Cs-fix

--- src/Video/Converter/AdapterInterface.php
+++ src/Video/Converter/AdapterInterface.php
@@ -19,9 +19,10 @@ interface AdapterInterface
     public function getMappedConversionParams(ConversionParamsInterface $conversionParams): array;
 
     /**
-     * @param array $arguments
+     * @param array       $arguments
      * @param null|string $inputFile
-     * @param null $outputFile
+     * @param null        $outputFile
+     *
      * @return string
      */
     public function getCliCommand(array $arguments, ?string $inputFile, $outputFile = null): string;
