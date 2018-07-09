24bdd001b247396947eeec4f27625e556581d687
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-09 14:55:29 +0200

    Refactored default config file

--- config/soluble-mediatools.global.php
+++ config/soluble-mediatools.global.php
@@ -3,18 +3,17 @@
 return [
     'soluble-mediatools' => [
 
-        /**
-         * Binaries
-         */
+        // FFMPEG
         'ffmpeg.binary'         => 'ffmpeg',   // or a complete path /opt/local/ffmpeg/bin/ffmpeg
-        'ffprobe.binary'        => 'ffprobe',  // or a complete path /opt/local/ffmpeg/bin/ffprobe
+        'ffmpeg.threads'        => null,       // <null>: single thread; <0>: number of cores, <1+>: number of threads
+        'ffmpeg.timeout'        => null,       // <null>: no timeout, <number>: number of seconds before timing-out
+        'ffmpeg.idle_timeout'   => 60,         // <null>: no idle timeout, <number>: number of seconds of inactivity before timing-out
+        'ffmpeg.env'            => [],         // An array of additional env vars to set when running the ffmpeg conversion process
 
-        /**
-         * Conversion service options
-         */
-        'conversion.threads'      => null,   // <null>: single thread; <0>: number of cores, <1+>: number of threads
-        'conversion.timeout'      => null,   // <null>: no timeout, <number>: number of seconds before timing-out
-        'conversion.idle_timeout' => 60,     // <null>: no idle timeout, <number>: number of seconds of inactivity before timing-out
-        'conversion.env'          => []      // An array of additional env vars to set when running the ffmpeg conversion process
+        // FFPROBE
+        'ffprobe.binary'        => 'ffprobe',  // or a complete path /opt/local/ffmpeg/bin/ffprobe
+        'ffprobe.timeout'       => null,       // <null>: no timeout, <number>: number of seconds before timing-out
+        'ffprobe.idle_timeout'  => 60,         // <null>: no idle timeout, <number>: number of seconds of inactivity before timing-out
+        'ffprobe.env'           => [],         // An array of additional env vars to set when running the ffprobe
     ],
 ];
