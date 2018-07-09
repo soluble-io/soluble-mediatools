5d86abcffbaea128cbf016ff34ce5dc30f82fbb4
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-09 16:31:20 +0200

    refactored for getSymfonyProcess()

--- src/Video/ThumbServiceInterface.php
+++ src/Video/ThumbServiceInterface.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Soluble\MediaTools\Video;
 
 use Soluble\MediaTools\Video\Filter\Type\VideoFilterInterface;
+use Symfony\Component\Process\Process;
 
 interface ThumbServiceInterface
 {
+    public function getSymfonyProcess(string $videoFile, string $thumbnailFile, ?SeekTime $time = null, ?VideoFilterInterface $videoFilter = null): Process;
+
     public function makeThumbnail(string $videoFile, string $thumbnailFile, ?SeekTime $time = null, ?VideoFilterInterface $videoFilter = null, ?callable $env = null): void;
 }
