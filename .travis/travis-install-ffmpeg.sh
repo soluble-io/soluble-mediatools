fe16dc743eb86a76327960df8dc3632eefaa685f
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-09 17:26:02 +0200

    updated install

--- .travis/travis-install-ffmpeg.sh
+++ .travis/travis-install-ffmpeg.sh
@@ -2,15 +2,15 @@
 # Small script to fetch a static ffmpeg
 set -ex
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 URL=https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 FILE=$(basename ${URL})
-DIST=${SCRIPT_DIR}/downloads
-DISTFILE=${DIST}/${FILE}
-DEST=${SCRIPT_DIR}/${FILE%%.*}
+DOWNLOAD_DIR=${BASE_DIR}/downloads
+DISTFILE=${DOWNLOAD_DIR}/${FILE}
+DEST=${BASE_DIR}/${FILE%%.*}
 
-[ -d ${DIST} ] || mkdir ${DIST}
+[ -d ${DOWNLOAD_DIR} ] || mkdir ${DOWNLOAD_DIR}
 [ -d ${DEST} ] || mkdir ${DEST}
 
 if [[ -f ${DISTFILE} ]]; then
