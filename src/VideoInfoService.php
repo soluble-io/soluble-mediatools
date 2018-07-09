7fa2fde4fded62f6693ed30e977fe9a2ec4f9cd8
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-09 14:56:46 +0200

    Use of symfony/process

--- src/VideoInfoService.php
+++ src/VideoInfoService.php
@@ -8,6 +8,7 @@ use Soluble\MediaTools\Config\FFProbeConfig;
 use Soluble\MediaTools\Exception\FileNotFoundException;
 use Soluble\MediaTools\Util\Assert\PathAssertionsTrait;
 use Soluble\MediaTools\Video\InfoServiceInterface;
+use Symfony\Component\Process\Process;
 
 class VideoInfoService implements InfoServiceInterface
 {
@@ -22,6 +23,39 @@ class VideoInfoService implements InfoServiceInterface
     }
 
     /**
+     * Return ready-to-run symfony process object that you can use
+     * to `run()` or `start()` programmatically. Useful if you want to make
+     * things your way...
+     *
+     * @see https://symfony.com/doc/current/components/process.html
+     *
+     * @throws FileNotFoundException when inputFile does not exists
+     */
+    public function getFFProbeProcess(string $inputFile): Process
+    {
+        $this->ensureFileExists($inputFile);
+
+        $ffprobeCmd = trim(sprintf(
+            '%s %s %s',
+            $this->ffprobeConfig->getBinary(),
+            implode(' ', [
+                '-v quiet',
+                '-print_format json',
+                '-show_format',
+                '-show_streams',
+            ]),
+            sprintf('-i %s', escapeshellarg($inputFile))
+        ));
+
+        $process = new Process($ffprobeCmd);
+        $process->setTimeout($this->ffprobeConfig->getTimeout());
+        $process->setIdleTimeout($this->ffprobeConfig->getIdleTimeout());
+        $process->setEnv($this->ffprobeConfig->getEnv());
+
+        return $process;
+    }
+
+    /**
      * @param string $file
      *
      * @return VideoInfo
@@ -31,23 +65,15 @@ class VideoInfoService implements InfoServiceInterface
      */
     public function getMediaInfo(string $file): VideoInfo
     {
-        $this->ensureFileExists($file);
-        $process = $this->ffprobeConfig->getProcess();
-        $cmd     = $process->buildCommand([
-            '-v quiet',
-            '-print_format json',
-            '-show_format',
-            '-show_streams',
-            sprintf('-i "%s"', $file),
-        ]);
+        $process = $this->getFFProbeProcess($file);
 
         try {
-            $jsonOutput = $process->runCommand($cmd);
+            $process->mustRun();
+            $output = $process->getOutput();
         } catch (\Throwable $e) {
-            //$msg = $e->getMessage();
             throw $e;
         }
 
-        return VideoInfo::createFromFFProbeJson($file, $jsonOutput);
+        return VideoInfo::createFromFFProbeJson($file, $output);
     }
 }
