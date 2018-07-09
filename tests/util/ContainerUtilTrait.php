4ffd4dc4e84708988e7e2c3662bf57b085ca5cb4
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-09 14:53:12 +0200

    Added utilities

---
+++ tests/util/ContainerUtilTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MediaToolsTest\Util;
+
+use Psr\Container\ContainerInterface;
+use Zend\ServiceManager\ServiceManager;
+
+trait ContainerUtilTrait
+{
+    public function createZendServiceManager(?array $mediaToolsConfig, string $entryName = 'config'): ContainerInterface
+    {
+        return new ServiceManager(
+            array_merge(
+                [
+                    'services' => [
+                        $entryName =>
+                            //(new ConfigProvider())->getDefaultConfiguration(),
+                            $mediaToolsConfig,
+                    ]],
+                [] //could be: (new ConfigProvider)->getDependencies()
+            )
+        );
+    }
+}
