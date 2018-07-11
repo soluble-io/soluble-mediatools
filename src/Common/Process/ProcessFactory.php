7f1b555058f334defc176833dd5f1248ce0922a9
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-07-11 11:55:14 +0200

    Added ProcessFactory

---
+++ src/Common/Process/ProcessFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Soluble\MediaTools\Common\Process;
+
+use Symfony\Component\Process\Process;
+
+class ProcessFactory
+{
+    /** @var string|array */
+    protected $command;
+
+    /** @var null|ProcessParamsInterface */
+    protected $processParams;
+
+    /**
+     * @param array|string                $command
+     * @param null|ProcessParamsInterface $processParams
+     */
+    public function __construct($command, ?ProcessParamsInterface $processParams = null)
+    {
+        $this->command       = $command;
+        $this->processParams = $processParams;
+    }
+
+    public function __invoke(): Process
+    {
+        $process = new Process($this->command);
+        if ($this->processParams !== null) {
+            $process->setTimeout($this->processParams->getTimeout());
+            $process->setIdleTimeout($this->processParams->getIdleTimeout());
+            $process->setEnv($this->processParams->getEnv());
+        }
+
+        return $process;
+    }
+}
