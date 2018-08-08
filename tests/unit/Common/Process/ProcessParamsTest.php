3650fd353d51b2acc1c29a83c2a8fddf405af78f
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-08-08 11:51:37 +0200

    Added tests for process

---
+++ tests/unit/Common/Process/ProcessParamsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MediaToolsTest\Common\Process;
+
+use PHPUnit\Framework\TestCase;
+use Soluble\MediaTools\Video\ProcessParams;
+
+class ProcessParamsTest extends TestCase
+{
+    public function testEnv(): void
+    {
+        $env = ['param' => 'value'];
+
+        $pp = new ProcessParams(null, null, $env);
+
+        self::assertSame($env, $pp->getEnv());
+
+        $newEnv = ['param' => 'value'];
+
+        $pp->setEnv($newEnv);
+
+        self::assertSame($newEnv, $pp->getEnv());
+    }
+
+    public function testTimeouts(): void
+    {
+        $pp = new ProcessParams(60.99, 10.1);
+
+        self::assertSame(60.99, $pp->getTimeout());
+        self::assertSame(10.1, $pp->getIdleTimeout());
+
+        $pp->setTimeout(1.0);
+        $pp->setIdleTimeout(1.1);
+
+        self::assertSame(1.0, $pp->getTimeout());
+        self::assertSame(1.1, $pp->getIdleTimeout());
+    }
+}
