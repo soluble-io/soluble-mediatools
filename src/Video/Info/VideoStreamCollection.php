3b2ed545cf2061d2a436c520c8c7d65ee5564874
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-02-20 12:01:55 +0100

    new methods

--- src/Video/Info/VideoStreamCollection.php
+++ src/Video/Info/VideoStreamCollection.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Soluble\MediaTools\Video\Info;
 
+use Soluble\MediaTools\Video\Exception\StreamNotFoundException;
+
 class VideoStreamCollection implements StreamCollectionInterface
 {
-    /**
-     * @var array<int, array>
-     */
+    /** @var array<int, array> */
     private $streamsMetadata;
+
+    /** @var array<int, VideoStream> */
+    private $streams;
 
     /**
      * @param array<int, array> $videoStreamsMetadata
@@ -17,10 +20,15 @@ class VideoStreamCollection implements StreamCollectionInterface
     public function __construct(array $videoStreamsMetadata)
     {
         $this->streamsMetadata = $videoStreamsMetadata;
+        $this->loadStreams();
     }
 
     public function getFirst(): VideoStream
     {
+        if ($this->count() === 0) {
+            throw new StreamNotFoundException('Unable to get video first stream, none exists');
+        }
+
         return new VideoStream($this->streamsMetadata[0]);
     }
 
@@ -31,6 +39,14 @@ class VideoStreamCollection implements StreamCollectionInterface
 
     public function getIterator(): \ArrayIterator
     {
-        return new \ArrayIterator($this->streamsMetadata);
+        return new \ArrayIterator($this->streams);
+    }
+
+    private function loadStreams(): void
+    {
+        $this->streams = [];
+        foreach ($this->streamsMetadata as $idx => $metadata) {
+            $this->streams[] = new VideoStream($metadata);
+        }
     }
 }
