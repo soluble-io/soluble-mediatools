a7ab3f6e3d36edbc544848e6ba52799e9200c576
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-02-20 12:01:13 +0100

    New exception to deal with empty streams

---
+++ src/Video/Exception/StreamNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @see       https://github.com/soluble-io/soluble-mediatools for the canonical repository
+ *
+ * @copyright Copyright (c) 2018-2019 Sébastien Vanvelthem. (https://github.com/belgattitude)
+ * @license   https://github.com/soluble-io/soluble-mediatools/blob/master/LICENSE.md MIT
+ */
+
+namespace Soluble\MediaTools\Video\Exception;
+
+class StreamNotFoundException extends \Soluble\MediaTools\Common\Exception\RuntimeException implements InfoReaderExceptionInterface
+{
+}
