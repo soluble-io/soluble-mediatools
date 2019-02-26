4a7888ce3215feb8fc480f1be19cae1d99a15b29
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-02-26 11:34:56 +0100

    Added getDimension and more doc

--- src/Video/Info/StreamInterface.php
+++ src/Video/Info/StreamInterface.php
@@ -6,10 +6,19 @@ namespace Soluble\MediaTools\Video\Info;
 
 interface StreamInterface
 {
+    /**
+     * Return stream index.
+     */
     public function getIndex(): int;
 
+    /**
+     * Return the codec type, either video, audio or data.
+     */
     public function getCodecType(): string;
 
+    /**
+     * Return the codec name: h264, vp8, vp9, h265, av1, opus, aac, mp3...
+     */
     public function getCodecName(): string;
 
     public function getCodecLongName(): ?string;
@@ -22,18 +31,28 @@ interface StreamInterface
 
     public function getDurationTs(): ?int;
 
+    /**
+     * Return stream duration in seconds.
+     */
     public function getDuration(): float;
 
     public function getProfile(): ?string;
 
+    /**
+     * Return stream bitrate if available (depends on encoder params).
+     */
     public function getBitRate(): ?int;
 
     /**
+     * Return tags attached to this stream.
+     *
      * @return array<string, string>
      */
     public function getTags(): array;
 
     /**
+     * Return underlying ffprobe stream metadata.
+     *
      * @return array<string, mixed>
      */
     public function getStreamMetadata(): array;
---
+++ src/Video/Info/StreamTypeInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Soluble\MediaTools\Video\Info;
+
+interface StreamTypeInterface
+{
+    public const AUDIO = 'audio';
+    public const VIDEO = 'video';
+    public const DATA  = 'data';
+}
