438b0a989c0835663adfb8664f3f9a5584a65ed7
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-04-10 16:23:34 +0200

    docs: updated copyright year

--- src/Video/Filter/Type/VideoFilterInterface.php
+++ src/Video/Filter/Type/VideoFilterInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * @see       https://github.com/soluble-io/soluble-mediatools for the canonical repository
  *
- * @copyright Copyright (c) 2018-2019 Sébastien Vanvelthem. (https://github.com/belgattitude)
+ * @copyright Copyright (c) 2018-2020 Sébastien Vanvelthem. (https://github.com/belgattitude)
  * @license   https://github.com/soluble-io/soluble-mediatools/blob/master/LICENSE.md MIT
  */
 
